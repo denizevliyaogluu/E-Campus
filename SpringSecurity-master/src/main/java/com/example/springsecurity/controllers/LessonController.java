53ed3c17ac59eba8586e1ff5f8eb7d8b1d5fb7f6
Deniz Evliyaoglu <112091753+denizevliyaogluu@users.noreply.github.com>
2022-12-27 21:00:29 +0300

    Update LessonController.java

--- SpringSecurity-master/src/main/java/com/example/springsecurity/controllers/LessonController.java
+++ SpringSecurity-master/src/main/java/com/example/springsecurity/controllers/LessonController.java
@@ -1,4 +1,5 @@
 package com.example.springsecurity.controllers;
+import com.example.springsecurity.models.Faculty;
 import com.example.springsecurity.models.Lesson;
 import com.example.springsecurity.services.LessonService;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Collection;
+import java.util.List;
 
 //ogrenci: ders listesi görüntüleme
 //ogrenci isleri: ders ekleme, ders cikarma, ders kaydı olusturma
@@ -23,13 +26,35 @@ public class LessonController {
     }
 
 
-    @Secured("ROLE_OGRENCIISLERI")
+
     @GetMapping()
     public ResponseEntity<?> getAllLessons(){
         return ResponseEntity.ok(lessonService.getAllLessons());
     }
 
+    @Secured("ROLE_OGRENCIISLERI")
+    @PostMapping()
+    public ResponseEntity<Lesson> addLesson(@RequestBody Lesson lesson){
+        lessonService.addLesson(lesson);
+        return ResponseEntity.ok(lesson);
+    }
+    @Secured("ROLE_OGRENCIISLERI")
+    @DeleteMapping("{id}")
+    public ResponseEntity<Object> deleteLesson(@PathVariable(value = "id")Long id){
+        lessonService.deleteLesson(id);
+        return ResponseEntity.noContent().build();
+    }
 
+   /* @GetMapping("{id}")
+    public List<Lesson> getLessonByDepartmentId(@PathVariable Long id){
+        return lessonService.getLessonByDepartmentId(id);
+    }*/
+   /* @Secured("ROLE_OGRENCIISLERI")
+    @GetMapping("{id}")
+    public List<Lesson> getLessonByStudentId(@PathVariable Long id){
+        return lessonService.getLessonByStudentId(id);
+    }*/
 
 
 }
+
