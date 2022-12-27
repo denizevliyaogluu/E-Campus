6b4d3b433871d46456455b6b93e20f8490663a59
Deniz Evliyaoglu <112091753+denizevliyaogluu@users.noreply.github.com>
2022-12-27 21:03:08 +0300

    Update DepartmentService.java

--- SpringSecurity-master/src/main/java/com/example/springsecurity/services/DepartmentService.java
+++ SpringSecurity-master/src/main/java/com/example/springsecurity/services/DepartmentService.java
@@ -4,6 +4,7 @@ import com.example.springsecurity.models.Department;
 import com.example.springsecurity.models.Lesson;
 import com.example.springsecurity.repos.DepartmentRepository;
 import com.example.springsecurity.repos.LessonRepository;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -18,18 +19,23 @@ public class DepartmentService {
         this.departmentRepository = departmentRepository;
         this.lessonRepository = lessonRepository;
     }
+
+    public List<Department> getAllDepartments(){
+        return departmentRepository.findAll();
+    }
     public void addDepartment(Department department){
         departmentRepository.save(department);
     }
-    public List<Department> getDepartmentByFaculty(Long facultyId ){
+    public List<Department> getDepartmentByFacultyId(Long facultyId ){
         return departmentRepository.findAllByFacultyId(facultyId);
     }
-    public void addLesson(Lesson lesson, Long departmentId){
+    public void addLessonByDepartmentId(Lesson lesson, Long departmentId){
         lesson.setDepartment(departmentRepository.findById(departmentId).get());
         lessonRepository.save(lesson);
     }
-    public Collection<Lesson> getLessonByDepartmentId(Long departmentId){
-        return departmentRepository.findById(departmentId).get().getLessons();
+    public void deleteDepartment(Long id){
+        departmentRepository.deleteById(id);
     }
 
 }
+
