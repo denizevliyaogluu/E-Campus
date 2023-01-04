a04747269ad4fe010e7b2d1bbecfe406c02f901f
Deniz Evliyaoglu <112091753+denizevliyaogluu@users.noreply.github.com>
2023-01-04 20:20:52 +0300

    Update InstituteController.java

--- SpringSecurity-master/src/main/java/com/example/springsecurity/controllers/InstituteController.java
+++ SpringSecurity-master/src/main/java/com/example/springsecurity/controllers/InstituteController.java
@@ -3,6 +3,7 @@ package com.example.springsecurity.controllers;
 import com.example.springsecurity.models.Department;
 import com.example.springsecurity.models.Faculty;
 import com.example.springsecurity.models.Institute;
+import com.example.springsecurity.models.Student;
 import com.example.springsecurity.services.InstituteService;
 import com.example.springsecurity.services.UserService;
 import org.springframework.http.ResponseEntity;
@@ -21,35 +22,29 @@ public class InstituteController {
 
         this.instituteService = instituteService;
     }
-    @Secured("ROLE_SISTEMYONETICISI")
+    //@Secured("ROLE_SISTEMYONETICISI")
     @GetMapping()
     public ResponseEntity<?> getAllInstitutes(){
         return ResponseEntity.ok(instituteService.getAll());
     }
 
-
-    /*@GetMapping("/{id}")
-    public List<Institute> GetInstituteByUserID(@PathVariable Long id)
-    {
-        return instituteService.getInstituteByUserId(id);
-    }*/
-
-    @Secured("ROLE_SISTEMYONETICISI")
+    //@Secured("ROLE_SISTEMYONETICISI")
     @PostMapping
     public ResponseEntity<Institute> addInstitutes(@RequestBody Institute institute){
         instituteService.addInstitute(institute);
         return ResponseEntity.ok(institute);
     }
-    @Secured("ROLE_SISTEMYONETICISI")
+    //@Secured("ROLE_SISTEMYONETICISI")
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteInstitute(@PathVariable(value = "id")Long id){
         instituteService.deleteInstitute(id);
         return ResponseEntity.noContent().build();
     }
 
-   /* @GetMapping("/{id}")
-    public List<Institute> getInstituteById(@PathVariable Long id){
-        return (List<Institute>) instituteService.getInstituteById(id);
-    }*/
+    //@Secured("ROLE_SISTEMYONETICISI")
+    @GetMapping("/{instituteId}")
+    public Institute getInstituteById(@PathVariable Long instituteId){
+        return instituteService.getInstituteById(instituteId);
+    }
 
 }
