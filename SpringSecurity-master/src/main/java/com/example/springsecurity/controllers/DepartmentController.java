f113205a73a53d192f3286ec40c8fda108b29ccc
Deniz Evliyaoglu <112091753+denizevliyaogluu@users.noreply.github.com>
2023-01-04 20:20:13 +0300

    Update DepartmentController.java

--- SpringSecurity-master/src/main/java/com/example/springsecurity/controllers/DepartmentController.java
+++ SpringSecurity-master/src/main/java/com/example/springsecurity/controllers/DepartmentController.java
@@ -7,6 +7,7 @@ import com.example.springsecurity.services.DepartmentService;
 import com.example.springsecurity.services.FacultyService;
 import com.example.springsecurity.services.InstituteService;
 import com.example.springsecurity.services.LessonService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
@@ -26,28 +27,42 @@ public class DepartmentController {
         this.facultyService = facultyService;
         this.instituteService = instituteService;
     }
-    @Secured("ROLE_SISTEMYONETICISI")
+   // @Secured("ROLE_SISTEMYONETICISI")
     @GetMapping()
     public ResponseEntity<List<Department>> getAllDepartments(){
         List<Department> departmentList = departmentService.getAllDepartments();
         return ResponseEntity.ok(departmentList);
     }
-    @Secured({"ROLE_SISTEMYONETICISI","ROLE_FAKULTEYONETIMI"})
+    //@Secured({"ROLE_SISTEMYONETICISI","ROLE_FAKULTEYONETIMI"})
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteDepartment(@PathVariable(value = "id")Long id){
         departmentService.deleteDepartment(id);
         return ResponseEntity.noContent().build();
     }
-    @Secured({"ROLE_SISTEMYONETICISI","ROLE_FAKULTEYONETIMI"})
-    @PostMapping()
+    //@Secured({"ROLE_SISTEMYONETICISI","ROLE_FAKULTEYONETIMI"})
+    @PostMapping
     public ResponseEntity<Department> addDepartment(@RequestBody Department department){
         departmentService.addDepartment(department);
         return ResponseEntity.ok(department);
     }
-    @Secured("ROLE_SISTEMYONETICISI")
-    @GetMapping("/faculty/{id}")
-    public List<Department> getDepartmentByFacultyId(@PathVariable Long id){
-        return departmentService.getDepartmentByFacultyId(id);
+    //@Secured("ROLE_SISTEMYONETICISI")
+    @GetMapping("/{departmentId}")
+    public Department getDepartmentById(@PathVariable Long departmentId){
+        return departmentService.getDepartmentById(departmentId);
+    }
+
+   // @PostMapping("/faculty/{id}")
+    @ResponseBody
+    public ResponseEntity<Department> addDepartmentByFacultyId(@PathVariable Long id, @RequestBody Department department){
+        return new ResponseEntity<>(departmentService.addDepartmentByFacultyId(id,department),
+                HttpStatus.CREATED);
+    }
+
+    //@PostMapping("/lesson/{id}")
+    @ResponseBody
+    public ResponseEntity<Department> addDepartmentByLessonId(@PathVariable Long id, @RequestBody Department department){
+        return new ResponseEntity<>(departmentService.addDepartmentByLessonId(id,department),
+               HttpStatus.CREATED);
     }
 
 
