202f57dbf3e04d9c9ea289433f6d532cfddc2957
Deniz Evliyaoglu <112091753+denizevliyaogluu@users.noreply.github.com>
2023-01-04 20:26:16 +0300

    Update StudentService.java

--- SpringSecurity-master/src/main/java/com/example/springsecurity/services/StudentService.java
+++ SpringSecurity-master/src/main/java/com/example/springsecurity/services/StudentService.java
@@ -1,7 +1,10 @@
 package com.example.springsecurity.services;
 
 
+import com.example.springsecurity.models.Department;
+import com.example.springsecurity.models.Lesson;
 import com.example.springsecurity.models.Student;
+import com.example.springsecurity.repos.LessonRepository;
 import com.example.springsecurity.repos.StudentRepository;
 import com.example.springsecurity.repos.UserRepository;
 import org.springframework.stereotype.Service;
@@ -12,10 +15,13 @@ import java.util.List;
 public class StudentService {
     private final UserRepository userRepository;
     private final StudentRepository studentRepository;
+    private final LessonRepository lessonRepository;
 
-    public StudentService(UserRepository userRepository, StudentRepository studentRepository) {
+    public StudentService(UserRepository userRepository, StudentRepository studentRepository, LessonRepository lessonRepository) {
         this.userRepository = userRepository;
         this.studentRepository = studentRepository;
+
+        this.lessonRepository = lessonRepository;
     }
 
     public void addStudent(Student student){
@@ -26,11 +32,20 @@ public class StudentService {
     }
 
     public List<Student> getAllStudents(){
+
         return studentRepository.findAll();
     }
-
     public void deleteStudent(Long id) {
         studentRepository.deleteById(id);
     }
-}
 
+    public Student getStudentById(Long id){
+        return studentRepository.findById(id).get();
+    }
+
+    public Student addStudentByLessonId(Long id, Student student){
+        student.setLesson(lessonRepository.findById(id).orElse(null));
+        return studentRepository.save(student);
+    }
+
+}
