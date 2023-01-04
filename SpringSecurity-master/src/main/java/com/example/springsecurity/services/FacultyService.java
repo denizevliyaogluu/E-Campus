dbb47d6ed5dd453ea2433b62f41e9b81b9e8d896
Deniz Evliyaoglu <112091753+denizevliyaogluu@users.noreply.github.com>
2023-01-04 20:25:20 +0300

    Update FacultyService.java

--- SpringSecurity-master/src/main/java/com/example/springsecurity/services/FacultyService.java
+++ SpringSecurity-master/src/main/java/com/example/springsecurity/services/FacultyService.java
@@ -1,12 +1,16 @@
 package com.example.springsecurity.services;
 import com.example.springsecurity.models.Department;
 import com.example.springsecurity.models.Faculty;
+import com.example.springsecurity.models.Lesson;
+import com.example.springsecurity.models.Personnel;
 import com.example.springsecurity.repos.DepartmentRepository;
 import com.example.springsecurity.repos.FacultyRepository;
+import com.example.springsecurity.repos.InstituteRepository;
+import com.example.springsecurity.repos.PersonnelRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.List;
 
 
@@ -17,31 +21,49 @@ public class FacultyService {
 
     private final FacultyRepository facultyRepository;
     private final DepartmentRepository departmentRepository;
+    private final InstituteRepository instituteRepository;
+    private final PersonnelRepository personnelRepository;
 
-    public FacultyService(FacultyRepository facultyRepository, DepartmentRepository departmentRepository){
+    public FacultyService(FacultyRepository facultyRepository, DepartmentRepository departmentRepository, InstituteRepository instituteRepository, PersonnelRepository personnelRepository){
         this.facultyRepository=facultyRepository;
         this.departmentRepository = departmentRepository;
+        this.instituteRepository = instituteRepository;
+        this.personnelRepository = personnelRepository;
     }
 
     public List<Faculty> getAllFaculty(){
         log.info("Get all faculties..");
         return facultyRepository.findAll();
     }
+
     public void addFaculty(Faculty faculty){
         facultyRepository.save(faculty);
     }
     public List<Faculty> getFacultyByInstituteId(Long instituteId){
         return facultyRepository.findAllByInstituteId(instituteId);
     }
+
     public void addDepartmentByFacultyId(Department department, Long facultyId){
         department.setFaculty(facultyRepository.findById(facultyId).get());
         departmentRepository.save(department);
     }
-    public Collection<Department> getDepartmentsByFacultyId(Long facultyId){
-        return facultyRepository.findById(facultyId).get().getDepartments();
-    }
+
+    @Secured("ROLE_SISTEMYONETICISI")
     public void deleteFaculty(Long id){
         facultyRepository.deleteById(id);
     }
-}
 
+    public Faculty getFacultyById(Long id){
+        return facultyRepository.findById(id).get();
+    }
+
+    public Faculty addFacultyByInstituteId(Long id, Faculty faculty){
+        faculty.setInstitute(instituteRepository.findById(id).orElse(null));
+        return facultyRepository.save(faculty);
+    }
+    public Personnel addPersonelByFacultyId(Long id,Personnel personnel){
+        personnel.setFaculty(facultyRepository.findById(id).orElse(null));
+        return personnelRepository.save(personnel);
+    }
+
+}
